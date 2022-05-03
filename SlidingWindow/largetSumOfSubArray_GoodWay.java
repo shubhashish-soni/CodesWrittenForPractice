d25af396ab551eaae662bed6de5f37acb7e0e632
shubhashish-soni <shubhashish.soni@hotmail.com>
2022-05-03 21:56:57 +0530

    LargestSumPossibleOfSubArray
    The Good way O(n)

--- SlidingWindow/largetSumOfSubArray_GoodWay.java
+++ SlidingWindow/largetSumOfSubArray_GoodWay.java
@@ -1,1 +1,24 @@
+import java.lang.*;
+import java.util.*;
+class SlidingWindow {
+    public int largestSum(List<Integer> list, Integer sizeOfSubArray){
+        Integer maxSum = Integer.MIN_VALUE;
+        Integer temp = 0;
+        for(int i = 0; i<sizeOfSubArray; i++) {
+            temp += list.get(i);
+        }
+        for(int i = sizeOfSubArray; i < list.size(); i++){
+            temp = temp + list.get(i) - list.get(i-sizeOfSubArray);
+            maxSum = Math.max(temp, maxSum);
+        }
+        return maxSum;
+    }
+}
 
+class Main {
+    public static void main(String args[]){
+        SlidingWindow s = new SlidingWindow();
+        List<Integer> list = List.of(1,2,3,4,122,2,4,2,11,12);
+        System.out.println(s.largestSum(list,new Integer(4)));
+    }
+}
